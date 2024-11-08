30b35aebb37eb70956901df0e65cedca60843b7a
ruslan30081 <rys.abdullin2002@mail.ru>
2024-11-08 15:03:18 +0500

    изменил на константы и удалил лишний файл

--- script.js
+++ script.js
@@ -1,12 +1,47 @@
+const title = "Название проекта";
+const screens = "Простые, Сложные, Интерактивные";
+const screenPrice = 1000;
+const rollback = 50;
+const fullPrice = 90000;
+const adaptive = true;
 
 
-//переменные
-let title = 'Монитор Samsung';
-let screens = 2;
-let screenPrice = 2000;
-let rollback = 10;
-let fullPrice = screenPrice * screens;
-let adaptive = fullPrice - fullPrice * rollback / 100;
+console.log(typeof title); 
+console.log(typeof fullPrice); 
+console.log(typeof adaptive); 
 
-alert(title);
-console.log('adaptive - ', adaptive);
+console.log(screens.length); 
+
+console.log(`Стоимость верстки экранов (${screenPrice}) рублей/ долларов/гривен/юани`);
+console.log(`Стоимость разработки сайта (${fullPrice}) рублей/ долларов/гривен/юани`);
+
+let screensArray = screens.toLowerCase().split(',');
+console.log(screensArray);
+
+let rollbackAmount = fullPrice * (rollback / 100); 
+console.log(`Процент отката посреднику за работу: ${rollbackAmount}`);
+
+
+// Усложненное задание
+
+// задание 1
+let num = 266219;
+
+// задание 2
+
+let a = 123;
+let product = 1;
+
+for (let i = 0; i < a.toString().length; i++) {
+  let digit = parseInt(a.toString()[i]); // преобразуем строку в число
+  product *= digit;
+}
+
+console.log(product); // 6
+
+// задание 3
+let pow = product ** 3;
+console.log(pow); //216
+
+// задание 4
+console.log(parseInt(pow.toString()[0]) + " " + parseInt(pow.toString()[1])); // 6
