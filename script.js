601db9f122818c47d37d37c282ba1422bddfae96
Ruslan <rys.abdullin2002@mail.ru>
2025-07-28 15:38:35 +0500

    ефакторинг: перенос всех функций в объект appData с методами start и logger

--- script.js
+++ script.js
@@ -1,41 +1,125 @@
 const appData = {
-    secretNumber: 0,
-
-    start() {
-        this.secretNumber = Math.floor(Math.random() * 100) + 1;
-        this.asking();
-        this.logger();
-    },
+    title: '',
+    service1: '',
+    servicePrice1: 0,
+    service2: '',
+    servicePrice2: 0,
+    screenType: '',
+    screenPrice: 0,
+    rollback: 50,
+    adaptive: false,
+    result: 0,
+    itemFullPrice: 0,
+    itemServicePercentPrice: 0,
+    itemAllServicePrices: 0,
 
     asking() {
-        let userInput = prompt("Угадай число от 1 до 100");
+        this.title = prompt("Введите название проекта");
 
-        if (userInput === null) {
-            alert("Игра окончена");
-            return;
-        }
+        do {
+            this.screenType = prompt("Выберите тип экрана:", ["Простые", "Сложные", "Интерактивные"]);
+            if (this.screenType === "Простые" || this.screenType === "Сложные" || this.screenType === "Интерактивные") {
+                console.log("screenType " + this.screenType);
+            } else {
+                alert("Неправильный выбор");
+            }
+        } while (this.screenType !== "Простые" && this.screenType !== "Сложные" && this.screenType !== "Интерактивные");
 
-        if (isNaN(userInput) || userInput.trim() === "") {
-            alert("Введи число!");
-            this.asking();
-            return;
-        }
+        do {
+            this.screenPrice = prompt("Сколько будет стоить данная работа?");
+            if (!isNaN(parseInt(this.screenPrice))) {
+                console.log("screenPrice " + parseInt(this.screenPrice));
+            } else {
+                alert("Введите число");
+            }
+        } while (isNaN(parseInt(this.screenPrice)));
 
-        userInput = Number(userInput);
+        do {
+            this.service1 = prompt("Какой дополнительный тип услуги нужен?");
+            if (this.service1 !== "") {
+                console.log("service1 " + this.service1);
+            } else {
+                alert("Пожалуйста, введите название услуги");
+            }
+        } while (this.service1 === "");
 
-        if (userInput > this.secretNumber) {
-            alert("Загаданное число меньше");
-            this.asking();
-        } else if (userInput < this.secretNumber) {
-            alert("Загаданное число больше");
-            this.asking();
+        do {
+            this.servicePrice1 = prompt("Сколько это будет стоить?");
+            if (!isNaN(parseInt(this.servicePrice1))) {
+                console.log("servicePrice1 " + parseInt(this.servicePrice1));
+            } else {
+                alert("Пожалуйста, введите стоимость услуги");
+            }
+        } while (isNaN(parseInt(this.servicePrice1)));
+
+        do {
+            this.service2 = prompt("Какой дополнительный тип услуги нужен?");
+            if (this.service2 !== "") {
+                console.log("service2 " + this.service2);
+            } else {
+                alert("Пожалуйста, введите название услуги");
+            }
+        } while (this.service2 === "");
+
+        do {
+            this.servicePrice2 = prompt("Сколько это будет стоить?");
+            if (!isNaN(parseInt(this.servicePrice2))) {
+                console.log("servicePrice2 " + parseInt(this.servicePrice2));
+            } else {
+                alert("Пожалуйста, введите стоимость услуги");
+            }
+        } while (isNaN(parseInt(this.servicePrice2)));
+
+        this.adaptive = confirm("Нужен ли адаптив на сайте?");
+    },
+
+    getAllServicePrices(a, b) {
+        return parseInt(a) + parseInt(b);
+    },
+
+    getFullPrice(screenPrice, allServicePrices) {
+        return parseInt(screenPrice) + parseInt(allServicePrices);
+    },
+
+    getTitle(title) {
+        title = title.toLowerCase();
+        title = title.replace(/^\s+/, "");
+        title = title[0].toUpperCase() + title.slice(1);
+        return title;
+    },
+
+    getServicePercentPrices(itemFullPrice, rollback) {
+        return Math.ceil(itemFullPrice - (itemFullPrice * rollback / 100));
+    },
+
+    showTypeOf(price) {
+        if (price === 0) {
+            console.log("Стоимость работы составляет 0 рублей");
+            console.log("Итоговая стоимость: 0 рублей");
+        } else if (price === 15000) {
+            console.log("Стоимость работы составляет 15000 рублей");
+            console.log("Итоговая стоимость: 15000 рублей");
+        } else if (price === 30000) {
+            console.log("Стоимость работы составляет 30000 рублей");
+            console.log("Итоговая стоимость: 30000 рублей");
+        } else if (price > 30000) {
+            console.log("Даем скидку в 10%");
+            const discount = price * 0.1;
+            const finalPrice = price - discount;
+            console.log("Итоговая стоимость: " + finalPrice);
+        } else if (price > 15000 && price < 30000) {
+            console.log("Даем скидку в 5%");
+            const discount = price * 0.05;
+            const finalPrice = price - discount;
+            console.log("Итоговая стоимость: " + finalPrice);
         } else {
-            alert("Поздравляю, Вы угадали!!!");
+            console.log("Скидка не предусмотрена");
+            console.log("Итоговая стоимость: " + price);
         }
     },
 
     logger() {
-        console.log("=== Информация об объекте appData ===");
+        console.log("=== ЛОГИРОВАНИЕ ОБЪЕКТА APPDATA ===");
         for (let key in this) {
             if (typeof this[key] === 'function') {
                 console.log(`Метод: ${key}`);
@@ -43,7 +127,30 @@ const appData = {
                 console.log(`Свойство: ${key} = ${this[key]}`);
             }
         }
-        console.log("=== Конец информации ===");
+        console.log("=== КОНЕЦ ЛОГИРОВАНИЯ ===");
+    },
+
+    start() {
+        this.asking();
+
+        const itemTitle = this.getTitle(this.title);
+        console.log(itemTitle);
+
+        this.result = this.getAllServicePrices(this.servicePrice1, this.servicePrice2);
+        console.log(this.result);
+
+        this.itemFullPrice = this.getFullPrice(this.screenPrice, this.result);
+        console.log(this.itemFullPrice);
+
+        this.itemServicePercentPrice = this.getServicePercentPrices(this.itemFullPrice, this.rollback);
+        console.log("стоимость за вычетом процента отката посреднику " + this.itemServicePercentPrice);
+
+        this.itemAllServicePrices = this.getAllServicePrices(this.servicePrice1, this.servicePrice2);
+        console.log(this.itemAllServicePrices);
+
+        this.showTypeOf(this.itemFullPrice);
+
+        this.logger();
     }
 };
 
